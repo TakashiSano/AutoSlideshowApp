4b7d180b7c82c534a61df9bc9a8ad4751794d83b
unknown <307156@t-gaia.co.jp>
2017-02-22 19:36:48 +0900

    最初のコミット

---
+++ app/src/main/java/jp/teacacademy/takahsi/sano/autoslideshowapp/MainActivity.java
@@ -0,0 +1,157 @@
+package jp.teacacademy.takahsi.sano.autoslideshowapp;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static jp.teacacademy.takahsi.sano.autoslideshowapp.R.id.button1;
+import static jp.teacacademy.takahsi.sano.autoslideshowapp.R.id.button2;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+
+    Button mButton1;
+    Button mButton2;
+    Button mButton3;
+    Cursor cursor;
+    Timer mTimer;
+    double mTimerSec = 0.0;
+    Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ContentResolver resolver = getContentResolver();
+        cursor = resolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                null, // 項目(null = 全項目)
+                null, // フィルタ条件(null = フィルタなし)
+                null, // フィルタ用パラメータ
+                null // ソート (null ソートなし)
+        );
+        if (cursor.moveToFirst()) {
+            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+            Long id = cursor.getLong(fieldIndex);
+            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+            ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+            imageVIew.setImageURI(imageUri);
+        };
+
+        mButton1 = (Button) findViewById(button1);
+        mButton2 = (Button) findViewById(button2);
+        mButton3 = (Button) findViewById(R.id.button3);
+
+
+
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cursor.moveToNext()){
+                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                    Long id = cursor.getLong(fieldIndex);
+                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                    ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                    imageVIew.setImageURI(imageUri);
+                }else{
+                    cursor.moveToFirst();
+                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                    Long id = cursor.getLong(fieldIndex);
+                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                    ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                    imageVIew.setImageURI(imageUri);
+                }
+            };
+        });
+
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cursor.moveToPrevious()) {
+                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                    Long id = cursor.getLong(fieldIndex);
+                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                    ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                    imageVIew.setImageURI(imageUri);
+                }else{
+                    cursor.moveToLast();
+                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                    Long id = cursor.getLong(fieldIndex);
+                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                    ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                    imageVIew.setImageURI(imageUri);
+                };
+            }
+        });
+
+        mButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTimer == null) {
+                    // タイマーの作成
+                    mTimer = new Timer();
+                    // タイマーの始動
+                    mTimer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            mTimerSec += 0.1;
+
+                            mHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (cursor.moveToNext()) {
+                                        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                                        Long id = cursor.getLong(fieldIndex);
+                                        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                                        ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                                        imageVIew.setImageURI(imageUri);
+                                    } else {
+                                        cursor.moveToFirst();
+                                        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                                        Long id = cursor.getLong(fieldIndex);
+                                        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                                        ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                                        imageVIew.setImageURI(imageUri);
+                                    }
+                                }
+                            });
+                        }
+                    }, 2000, 2000);
+                    mButton1.setEnabled(false);
+                    mButton2.setEnabled(false);
+                } else {
+                    if (mTimer != null) {
+                        mTimer.cancel();
+                        mTimer = null;
+                    }
+                    mButton1.setEnabled(true);
+                    mButton2.setEnabled(true);
+                };
+        };
+    });
+ }
+}
